8516db834db1ac5de9bbcc80012a7de949277072
moizali125 <ha9913083@gmail.com>
2024-03-14 22:59:06 +0500

    math expression

---
+++ app.js
@@ -0,0 +1,13 @@
+var tast1a = 'Sum of '
+var tast1b = 'and '
+var tast1c = 'is '
+document.write(tast1a + 3 + tast1b + 5 + tast1c + 8)
+var tast2a = 'value after variable declaration is'
+var a = 2345
+var tast2b = 'Initial value :5'
+var tast2c = 'value after increment is :6'
+var c = 7
+var tast2d = 'value after addition is:13'
+var tast2e = 'value after decrement is:12'
+var tast2e = 'the remainder is : 0'
+document.write(tast2a + tast2b + tast2c + tast2d + tast2e)
